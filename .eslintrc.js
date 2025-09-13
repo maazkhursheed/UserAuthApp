54e24bc48ac11994748df222ed4269b7b97abf50
maazdev101 <m.khursheed@soum.sa>
2025-09-13 16:58:09 +0500

    Initial code base with required architecture and elements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    '@react-native', 
+    'plugin:@typescript-eslint/recommended', 
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  ignorePatterns: ['node_modules/'],
 };
